9a30dd524721ce146e0c4516d6894d6b86c8eaaa
jason8105 <jasondsilva8197@gmail.com>
2026-07-29 13:44:13 +0530

    Auto-fix applied by Full-Auto master_heal.py

--- module/src/main/cpp/Android.mk
+++ module/src/main/cpp/Android.mk
@@ -3,8 +3,12 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := module
-LOCAL_SRC_FILES := main.cpp
+
+# Dynamically discover all .cpp and .c source files in the directory
+LOCAL_SRC_FILES := $(patsubst $(LOCAL_PATH)/%,%,$(wildcard $(LOCAL_PATH)/*.cpp $(LOCAL_PATH)/*.c))
+
 LOCAL_LDLIBS := -llog
-LOCAL_CPPFLAGS += -std=c++20
+LOCAL_CFLAGS := -Wall -O3
+LOCAL_CPPFLAGS := -std=c++20
 
 include $(BUILD_SHARED_LIBRARY)
