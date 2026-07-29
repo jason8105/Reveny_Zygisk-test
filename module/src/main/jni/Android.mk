f362ec5febfba791371582c3925d349b07abceeb
jason8105 <jasondsilva8197@gmail.com>
2026-07-30 02:27:27 +0530

    Update Android.mk

--- module/src/main/jni/Android.mk
+++ module/src/main/jni/Android.mk
@@ -3,7 +3,18 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := module
-LOCAL_SRC_FILES := main.cpp
-LOCAL_CPPFLAGS += -std=c++20
+
+LOCAL_CPPFLAGS := -std=c++17 -fpermissive -Wno-error -fdeclspec -fexceptions -frtti -fvisibility=hidden
+LOCAL_CFLAGS := -fvisibility=hidden -Wno-error
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH) \
+                    $(LOCAL_PATH)/../cpp \
+                    $(LOCAL_PATH)/../cpp/Headers
+
+LOCAL_SRC_FILES := \
+    ../cpp/Main.cpp \
+    ../cpp/ModMenu.cpp
+
+LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv2
 
 include $(BUILD_SHARED_LIBRARY)
