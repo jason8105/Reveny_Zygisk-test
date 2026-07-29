e321837e32b16357985a6146c085cfb069aac996
jason8105 <jasondsilva8197@gmail.com>
2026-07-30 02:11:48 +0530

    Auto-fix applied by Full-Auto master_heal.py

--- module/src/main/jni/Android.mk
+++ module/src/main/jni/Android.mk
@@ -3,16 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := module
-
-LOCAL_CPPFLAGS := -std=c++17 -fpermissive -Wno-error -fdeclspec -fexceptions -frtti -fvisibility=hidden
-LOCAL_CFLAGS := -fvisibility=hidden -Wno-error
-
-LOCAL_C_INCLUDES := $(LOCAL_PATH)
-
-LOCAL_SRC_FILES := \
-    Main.cpp \
-    ModMenu.cpp
-
-LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv2
+LOCAL_SRC_FILES := main.cpp
+LOCAL_CPPFLAGS += -std=c++20
 
 include $(BUILD_SHARED_LIBRARY)
