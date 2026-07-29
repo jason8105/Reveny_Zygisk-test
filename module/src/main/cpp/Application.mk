3f0069421a4a3111d379f15987842fcc410dbc96
jason8105 <jasondsilva8197@gmail.com>
2026-07-29 14:10:17 +0530

    Update Application.mk
    Fix Utility.cpp compile error: Bump APP_PLATFORM to android-23 for process_vm_readv support

--- module/src/main/cpp/Application.mk
+++ module/src/main/cpp/Application.mk
@@ -1,4 +1,6 @@
 APP_ABI := arm64-v8a armeabi-v7a
-APP_PLATFORM := android-21
+# Yahan android-21 ki jagah android-23 kar diya hai
+APP_PLATFORM := android-23
 APP_STL := c++_static
 APP_CPPFLAGS := -std=c++17 -fpermissive -Wno-error -fexceptions -frtti
+
