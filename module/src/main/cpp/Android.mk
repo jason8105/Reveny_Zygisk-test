9f393956f11d6257b6b5e850d6dfa5486fb22911
jason8105 <jasondsilva8197@gmail.com>
2026-07-29 14:03:38 +0530

    Update Android.mk

--- module/src/main/cpp/Android.mk
+++ module/src/main/cpp/Android.mk
@@ -1,9 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-# 1. Sabse pehle External libraries ko compile karne ke liye include karein
-include $(LOCAL_PATH)/External/Android.mk
-
-# 2. Main module ke variables clear karein
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := module
@@ -11,7 +7,7 @@ LOCAL_MODULE := module
 LOCAL_CPPFLAGS := -std=c++17 -fpermissive -Wno-error -fdeclspec -fexceptions -frtti -fvisibility=hidden
 LOCAL_CFLAGS := -fvisibility=hidden -Wno-error
 
-# 3. Saare folders ke paths yahan daalein taaki 'imgui.h' easily mil jaye
+# 1. Saare Includes
 LOCAL_C_INCLUDES := $(LOCAL_PATH) \
                     $(LOCAL_PATH)/Include \
                     $(LOCAL_PATH)/Headers \
@@ -19,17 +15,20 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH) \
                     $(LOCAL_PATH)/External/KittyMemory \
                     $(LOCAL_PATH)/External/Dobby
 
-# 4. Saari C++ files ko compile list mein add karein
+# 2. Main C++ Files
 LOCAL_SRC_FILES := \
     Main.cpp \
     ModMenu.cpp \
     Drawing.cpp \
     Utility.cpp
 
-# 5. Android ki default libraries
 LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv2
 
-# 6. External folder se aane wali static libraries ko link karein
+# 3. External Libraries Link
 LOCAL_STATIC_LIBRARIES := libimgui libkitty libdobby
 
 include $(BUILD_SHARED_LIBRARY)
+
+# 4. SABSE IMPORTANT: External Android.mk ko sabse last mein call karo!
+# Isse LOCAL_PATH overwrite nahi hoga aur files sahi jagah dhoondhi jayengi.
+include $(LOCAL_PATH)/External/Android.mk
