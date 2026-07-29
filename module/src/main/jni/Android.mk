79f63a7f25fc31051890e9231cf41c28a3471d85
jason8105 <jasondsilva8197@gmail.com>
2026-07-30 02:29:21 +0530

    Update Android.mk

--- module/src/main/jni/Android.mk
+++ module/src/main/jni/Android.mk
@@ -7,14 +7,28 @@ LOCAL_MODULE := module
 LOCAL_CPPFLAGS := -std=c++17 -fpermissive -Wno-error -fdeclspec -fexceptions -frtti -fvisibility=hidden
 LOCAL_CFLAGS := -fvisibility=hidden -Wno-error
 
+# 1. Saare Includes (ImGui aur Headers ke sath)
 LOCAL_C_INCLUDES := $(LOCAL_PATH) \
-                    $(LOCAL_PATH)/../cpp \
-                    $(LOCAL_PATH)/../cpp/Headers
+                    $(LOCAL_PATH)/Include \
+                    $(LOCAL_PATH)/Headers \
+                    $(LOCAL_PATH)/External/ImGui \
+                    $(LOCAL_PATH)/External/KittyMemory \
+                    $(LOCAL_PATH)/External/Dobby
 
+# 2. Main C++ Files
 LOCAL_SRC_FILES := \
-    ../cpp/Main.cpp \
-    ../cpp/ModMenu.cpp
+    Main.cpp \
+    ModMenu.cpp \
+    Drawing.cpp \
+    Utility.cpp
 
 LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv2
 
+# 3. External Libraries Link
+LOCAL_STATIC_LIBRARIES := libimgui libkitty libdobby
+
 include $(BUILD_SHARED_LIBRARY)
+
+# 4. External Android.mk call
+include $(LOCAL_PATH)/External/Android.mk
+
