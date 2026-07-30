e165a1a05836d41365b1e5c05eff11a7d0f08511
jason8105 <jasondsilva8197@gmail.com>
2026-07-30 11:43:14 +0530

    Auto-fix applied by Full-Auto master_heal.py

--- module/src/main/jni/Android.mk
+++ module/src/main/jni/Android.mk
@@ -3,8 +3,15 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := reveny
-LOCAL_SRC_FILES := main.cpp
-LOCAL_LDLIBS := -log -landroid
-LOCAL_CPPFLAGS := -std=c++17
+
+LOCAL_CPPFLAGS := -std=c++20 -fexceptions -frtti -Wall -Wno-error=format-security -Wno-unused-variable
+LOCAL_CFLAGS := -Wall -Wno-error=format-security
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)
+
+LOCAL_SRC_FILES := ModMenu.cpp \
+                   main.cpp
+
+LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv2 -lz
 
 include $(BUILD_SHARED_LIBRARY)
