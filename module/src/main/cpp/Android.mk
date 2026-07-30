b3812ab3574874af6bf545ff148a58e4999ba887
jason8105 <jasondsilva8197@gmail.com>
2026-07-30 11:40:46 +0530

    Auto-fix applied by Full-Auto master_heal.py

--- module/src/main/cpp/Android.mk
+++ module/src/main/cpp/Android.mk
@@ -2,9 +2,11 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := module
-LOCAL_SRC_FILES := main.cpp Utility.cpp
-LOCAL_LDLIBS := -llog
-LOCAL_CPPFLAGS += -std=c++17 -Wall
+LOCAL_MODULE := reveny
+LOCAL_SRC_FILES := $(patsubst $(LOCAL_PATH)/%,%,$(wildcard $(LOCAL_PATH)/*.cpp))
+LOCAL_C_INCLUDES := $(LOCAL_PATH)
+LOCAL_CPPFLAGS := -std=c++20 -O3 -Wall -fvisibility=hidden -fvisibility-inlines-hidden
+LOCAL_CFLAGS := -O3 -Wall -fvisibility=hidden
+LOCAL_LDLIBS := -llog -landroid -ldl
 
 include $(BUILD_SHARED_LIBRARY)
--- module/src/main/cpp/Application.mk
+++ module/src/main/cpp/Application.mk
@@ -1,3 +1,6 @@
-APP_STL := c++_static
-APP_CPPFLAGS := -std=c++17
+APP_ABI := arm64-v8a armeabi-v7a x86 x86_64
 APP_PLATFORM := android-26
+APP_STL := c++_static
+APP_CPPFLAGS := -std=c++20 -O3 -Wall -Wextra
+APP_CFLAGS := -O3 -Wall
+APP_LDFLAGS := -Wl,--gc-sections
